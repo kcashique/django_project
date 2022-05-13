ab0c1f4687c724eac7af5ac0b1411e341cab22e6
Ashiquekc <Ashiquekc97@gmail.com>
2022-05-13 14:59:12 +0530

    up

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from core.custom_admin import CustomAdmin
-from .models import Job, Location
+from .models import Job, Country, State
 
 
 @admin.register(Job)
 class JobAdmin(CustomAdmin):
     pass
 
-    @admin.register(Location)
-    class LocationAdmin(CustomAdmin):
-        pass
+@admin.register(Country)
+class CountryAdmin(CustomAdmin):
+    pass
+
+@admin.register(State)
+class StateAdmin(CustomAdmin):
+    pass
